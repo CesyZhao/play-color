322caa36527fca4467b5c95ea6dc3d2c28af2069
chris.zhao <chris.zhao@greaconsulting.com>
2019-03-25 08:56:01 +0800

    axios interceptor 配置

---
+++ src/http.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  xhrFields: {
+    withCredentials: true
+  },
+  timeout:50000
+})
+instance.interceptors.response.use((resp) => {
+  return resp
+},(error) => {
+  if(error.response){
+    switch(error.response.status){
+      case 401:
+
+        break
+      case 403:
+
+        break
+      case 404:
+
+        break
+      case 500:
+
+        break
+    }
+  }
+  return Promise.reject(error)
+})
+export default instance
