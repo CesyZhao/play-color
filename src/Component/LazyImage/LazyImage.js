8e52f5167166f1d19573cdde27bbb165fb84c2e5
chris.zhao <chris.zhao@greaconsulting.com>
2019-03-27 19:48:21 +0800

    样式

---
+++ src/Component/LazyImage/LazyImage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class LazyImage extends Component{
+  render() {
+    return (
+      <div className='pl-lazy-image-wrapper'>
+
+      </div>
+    )
+  }
+}
+
+export default LazyImage
