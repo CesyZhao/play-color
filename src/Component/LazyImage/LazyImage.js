68d5efab4ccba3a8499d10dccddbb8a5c0e0d9fe
chris.zhao <chris.zhao@greaconsulting.com>
2019-03-28 09:03:21 +0800

    添加 prop-types 库; 新增 LazyImage 组件

--- src/Component/LazyImage/LazyImage.js
+++ src/Component/LazyImage/LazyImage.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class LazyImage extends Component{
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    imgUrl: PropTypes.string.isRequired
+  }
+  static defaultProps = {
+    shape: 'squared'
+  }
+
   render() {
+    const style = {
+      width: `${this.props.width}px`,
+      height: `${this.props.height}px`
+    }
     return (
-      <div className='pl-lazy-image-wrapper'>
+      <div className={`pl-lazy-image-wrapper ${this.props.shape}`} style={ style }>
 
       </div>
     )
