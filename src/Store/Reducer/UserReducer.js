6a47335f0475d83cfd79b3faffd7daf7f4ad564b
chris <chris.zhao@greaconsulting.com>
2019-04-09 20:58:46 +0800

    redux

--- src/Store/Reducer/UserReducer.js
+++ src/Store/Reducer/UserReducer.js
@@ -1,7 +1,12 @@
+import { GET_USER_INFO } from '../Action/actions'
+
 const initState = {
   userInfo: {}
 }
 
 export default function UserReducer (state = initState, action) {
-
+  switch (action.type) {
+    case GET_USER_INFO:
+      return state
+  }
 }
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,2 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
+import userReducer from './Reducer/UserReducer'
 
+const reducers = {
+  user: userReducer
+}
+
+const rootReducer = combineReducers(reducers)
+
+const store = createStore(rootReducer)
