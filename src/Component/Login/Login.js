30f135c0f1753a5f87f074c8ce30d57074ccd3f2
chris.zhao <chris.zhao@greaconsulting.com>
2019-04-16 09:00:18 +0800

    canvas

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react'
 import './Login.less'
 
+const defaultSettings = {
+  WAVE_HEIGHT: 200, //波浪变化高度
+  SCALE : 0.5, // 绘制速率
+  CYCLE : 360, // SCALE
+  TIME : 0
+}
+
 class Login extends Component {
   render() {
     return (
       <div className='pc-login'>
-
+        <canvas className='pc-login-canvas' ref='canvas'></canvas>
       </div>
     )
   }
