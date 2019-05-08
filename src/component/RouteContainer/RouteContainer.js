5f47db4a68bd3dbb3cbfe0eaf3f94b232e8e7cc0
chris.zhao <chris.zhao@greaconsulting.com>
2019-05-08 19:56:39 +0800

    icons

--- src/component/RouteContainer/RouteContainer.js
+++ src/component/RouteContainer/RouteContainer.js
@@ -5,7 +5,7 @@ class RouteContainer extends Component{
   render() {
     return(
       <div className="pc-route-container">
-
+        <div className="pc-tool-bar"></div>
       </div>
     )
   }
