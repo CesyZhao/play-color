7c2f1ee84e7ebbca8ba1f1dc3918738845983b15
Chris Zhao <997010638@qq.com>
2019-05-23 09:07:17 +0800

    主页 banners

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,23 +3,35 @@ import './Home.less'
 import http from '../../config/http'
 // eslint-disable-next-line no-unused-vars
 import LazyImage  from '../LazyImage/LazyImage'
+import SwipeableViews from 'react-swipeable-views'
+import { autoPlay } from 'react-swipeable-views-utils'
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
 class Home extends Component {
 
   state = {
-    albumList: []
+    albumList: [],
+    banners: []
   }
 
   async componentWillMount () {
     let res = await http.get('/personalized')
     this.setState({albumList: res.data.result})
+    let bannerRes = await http.get('/banner')
+    this.setState({banners: bannerRes.data.banners})
+    console.log(bannerRes)
   }
 
   render() {
     return (
       <div className="pc-home">
         <div className='pc-home-banner'>
-
+          <AutoPlaySwipeableViews>
+            {
+              this.state.banners.map(banner => <img src={banner.imageUrl} alt='banner'></img>)
+            }
+          </AutoPlaySwipeableViews>
         </div>
         {
           this.state.albumList.map(album => {
