4b86851a487be780b9e97c2411e83621b4cc3476
Chris Zhao <997010638@qq.com>
2019-07-02 09:04:12 +0800

    所有歌单

--- src/component/Albums/Albums.js
+++ src/component/Albums/Albums.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react'
 import './Albums.less'
+import http from '../../config/http'
+import _ from 'lodash'
 
 class Albums extends Component {
+
+  state = {
+    catList: [],
+    hotList: []
+  }
+
+  async componentWillMount () {
+    try {
+      const { data: catList } = await http.get('/playlist/catlist')
+      const { data: hotList } = await http.get('/playlist/hot')
+      console.log(hotList)
+      this.setState({ catList: _.groupBy(catList.sub, cat => cat.category), hotList: hotList.tags })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render () {
     return (
       <div className="pc-albums">
         <div className="pc-albums-tool-bar">
-          <div className="pc-albums-categories"></div>
+          <div className="pc-albums-categories">
+            <span></span>
+            {
+              this.state.hotList.map(tag => {
+                return <span key={tag.id} className='pc-albums-hot-category'>
+                  {tag.name}
+                </span>
+              })
+            }
+          </div>
           <div className="pc-albums-layout"></div>
         </div>
       </div>
