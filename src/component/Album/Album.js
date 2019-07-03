c4085d0b6ba616e1123c4b728f683b5485f5ac08
Chris Zhao <997010638@qq.com>
2019-07-03 09:02:28 +0800

    歌单页

--- src/component/Album/Album.js
+++ src/component/Album/Album.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react'
+import http from '../../config/http'
+import LazyImage from '../LazyImage/LazyImage'
+import './Album.less'
 
 class Album extends Component {
-  componentDidMount () {
-    console.log(this.props.match.params.id)
+  state = {
+    album: null
   }
-  render() {
-    return (
-      <div>
 
+  async componentDidMount () {
+    try {
+      const { data } = await http.get(`/playlist/detail?id=${this.props.match.params.id}`)
+      console.log(data.playlist)
+      this.setState({ album: data.playlist })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  render() {
+    const album = this.state.album
+    console.log(album, '---------')
+    return (
+      album &&
+      <div>
+        <div className='pc-album-info'>
+          <LazyImage imgUrl={ this.state.album.coverImgUrl  }></LazyImage>
+        </div>
       </div>
     )
   }
