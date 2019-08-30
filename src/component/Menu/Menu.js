8bc008f934fb0b84372aaa9a5f76bcbf2867b66a
Chris Zhao <997010638@qq.com>
2019-08-30 17:00:55 +0800

    菜单样式

--- src/component/Menu/Menu.js
+++ src/component/Menu/Menu.js
@@ -38,6 +38,22 @@ class Leftbar extends Component {
     }
     this.setState({
       showMenu: !this.state.showMenu
+    }, () => {
+      if (this.state.showMenu) {
+        const canvas = document.querySelector('#menuCanvas')
+        const context = canvas.getContext('2d')
+        let grad = context.createLinearGradient(740, 0, 964, 608)//创建一个渐变色线性对象
+        grad.addColorStop(0,"#4E8291")             //定义渐变色颜色
+        grad.addColorStop(1,"#51416B")
+        context.moveTo( 964, 0 )
+        context.lineTo( 740, 0 )
+        context.quadraticCurveTo(800, 304, 664, 608)
+        context.lineTo( 964, 608 )
+        context.fillStyle = grad
+        context.shadowBlur = 30
+        context.shadowColor = "#51416B"
+        context.fill()
+      }
     })
     EventBus.emit('closeMenu')
   }
@@ -46,34 +62,38 @@ class Leftbar extends Component {
     return (
       <CSSTransition in={this.state.showMenu} timeout={300} unmountOnExit classNames="pc-leftbar">
         <div className='pc-leftbar'>
-          <div className='pc-leftbar-logo'>
-            { profile ? 
-              <React.Fragment>
-                <img src={profile.avatarUrl} alt="用户头像" className="pc-user-avatar"  onClick={ this.toggleLogin }/>
-                <span  onClick={ this.toggleLogin }>{profile.nickname}</span>
-              </React.Fragment>
-              : <React.Fragment>
-                  <img src={logo} alt="logo" onClick={ this.toggleLogin }/>
-                  <span onClick={ this.toggleLogin }>PLAY COLOR</span> 
-                </React.Fragment>
-             }
-          </div>
-          {
-            menu.map((category,index) => {
-              const item = category.list.map(item => 
-                <Link to={item.link}  key={item.name} onClick={ () => this.toggleMenu(item.name) }>
-                  <div className='pc-leftbar-category-item'>
-                    <i className={`iconfont ${item.icon}`} />
-                    <span>{item.name}</span>
+          <canvas id="menuCanvas" width="964" height="608"></canvas>
+          <div className="menuWrapper">
+            {
+              menu.map((category,index) => {
+                const item = category.list.map(item => 
+                  <Link to={item.link}  key={item.name} onClick={ () => this.toggleMenu(item.name) }>
+                    <div className='pc-leftbar-category-item'>
+                      <i className={`iconfont ${item.icon}`} />
+                      <span>{item.name}</span>
+                    </div>
+                  </Link> )
+                return (
+                  <div className='pc-leftbar-category' key={index}>
+                    {category.name && <div className='pc-leftbar-category-label'>{ category.name }</div>} 
+                    { item }
                   </div>
-                </Link> )
-              return (
-                <div className='pc-leftbar-category' key={index}>
-                  { item }
-                </div>
-              )
-            })
-          }
+                )
+              })
+            }
+            <div className='pc-leftbar-logo'>
+              { profile ? 
+                <React.Fragment>
+                  <img src={profile.avatarUrl} alt="用户头像" className="pc-user-avatar"  onClick={ this.toggleLogin }/>
+                  <span  onClick={ this.toggleLogin }>{profile.nickname}</span>
+                </React.Fragment>
+                : <React.Fragment>
+                    <img src={logo} alt="logo" onClick={ this.toggleLogin }/>
+                    <span onClick={ this.toggleLogin }>PLAY COLOR</span> 
+                  </React.Fragment>
+              }
+            </div>
+          </div>
         </div>
         </CSSTransition>
     )
