2789ff732181dca3574417b17294b7eb00d633a3
Chris Zhao <997010638@qq.com>
2019-09-12 08:51:31 +0800

    搜索

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -9,6 +9,10 @@ class Search extends Component {
           <input className="pc-search-input" placeholder="输入搜索关键字"></input>
           <i className="iconfont icon-sousuo1"></i>
         </div>
+        <div className="pc-search-results-wrapper">
+          <div className="pc-search-results"></div>
+          <div className="pc-search-result"></div>
+        </div>
       </div>
     ) 
   }
