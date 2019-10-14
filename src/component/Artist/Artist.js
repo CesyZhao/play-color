1d5dd5249b7516773702960e3bba2bd4ecd128f4
Chris Zhao <997010638@qq.com>
2019-10-14 08:59:17 +0800

    歌手详情页

--- src/component/Artist/Artist.js
+++ src/component/Artist/Artist.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react'
+import './Artist.less'
+import http from '../../config/http'
 
+const categories = ['song', 'mv', 'album', 'desc']
 class Artist extends Component {
 
   state = {
-    artistInfo: {},
-    artistTop50: [],
-    artistMvs: [],
-    artistAlbums: []
+    desc: '',
+    song: [],
+    mv: [],
+    album: []
   }
 
   componentWillMount () {
-
+  
   }
 
-  getArtistInfo () {
-    
+  getResultByType = (type) => {
+    const { id } = this.props.match.params
+    try {
+      http.get(`/artist/${type}?id=${id}`)
+      .then(({data}) => {
+        console.log(data)
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
+
 
   render () {
     return (
       <div className="pc-artist">
-        {
-          this.props.match.params.id
-        }
+        <div className="pc-artist-info">
+
+        </div>
+
       </div>
     )
   }
