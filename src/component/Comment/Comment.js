b6d0c0ec8d1480c3799f013ff915d0a477fd9fce
Chris Zhao <997010638@qq.com>
2019-11-11 18:50:47 +0800

    comment

--- src/component/Comment/Comment.js
+++ src/component/Comment/Comment.js
@@ -11,6 +11,12 @@ class Comment extends Component {
   state = {
     comment: {}
   }
+  async componentWillMount () {
+    const comment = await this.getComments(this.props.controller.song)
+    this.setState({
+      comment
+    })
+  }
   async componentDidUpdate (prevProps, prevState) {
     if (this.props.controller.song.id !== prevProps.controller.song.id) {
       const comment = await this.getComments(this.props.controller.song)
@@ -32,6 +38,7 @@ class Comment extends Component {
   }
   render () {
     const { song } = this.props.controller
+    const { comment } = this.state
     return (
       <div className='pc-comment'>
         <div className='pc-song-info'>
@@ -43,6 +50,24 @@ class Comment extends Component {
               <span className='pc-song-info-tag'>专辑: { song.album.name } </span>
             </div>
             <div className='pc-song-info-tag'> { song.name } </div>
+          </div>
+        </div>
+        <div className='pc-comment-detail'>
+          <div className='pc-comment-writer'>
+            <div className='pc-comment-header'>
+              <span> 评论 </span>
+              <span> { comment.total } 条</span>
+            </div>
+            <div className='pc-comment-input'>
+              <input></input>
+              <span> 发布 </span>
+            </div>
+          </div>
+          <div className='pc-comment-wrapper'>
+            <div className='pc-comment-hot'>
+              
+            </div>
+            <div className='pc-comment-newest'></div>
           </div>
         </div>
       </div>
