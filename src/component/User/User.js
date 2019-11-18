f4a922b1c93fe365746c43d1e7d33a54aa5abdf0
Chris Zhao <997010638@qq.com>
2019-11-18 08:53:07 +0800

    userprofile

--- src/component/User/User.js
+++ src/component/User/User.js
@@ -1,12 +1,32 @@
 import React, { Component } from  'react'
 import './User.less'
+import http from '../../config/http'
+import _ from 'lodash'
 
 class User extends Component {
+  state = {
+    user: {}
+  }
+  async componentWillMount () {
+    const { data } = await http.get(`/user/detail?uid=${this.props.match.params.id}`)
+    this.setState({ user: data})
+  }
   render () {
+    const { user } = this.state
+    console.log(user)
     return (
+      !_.isEmpty(user)
+      ?
       <div className='pc-user'>
-        
+        <div className='pc-user-profile'>
+          <div className='pc-user-avatar'>
+            <img src={ user.profile.avatarUrl }></img>
+          </div>
+          <div className='pc-user-info'></div>
+        </div>
       </div>
+      :
+      <div></div>
     )
   }
 }
