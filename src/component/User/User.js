c96d59cdf052bb7a913c7b7d1d1db055c5222d30
Chris Zhao <997010638@qq.com>
2019-11-21 08:40:29 +0800

    user

--- src/component/User/User.js
+++ src/component/User/User.js
@@ -31,8 +31,19 @@ class User extends Component {
               <span className='pc-user-level'>Lv.{ user.level } </span>
               <span className='pc-user-level'><i className={ `iconfont ${ user.profile.gender === 1 ? 'icon-nan male' : 'icon-nv female' }` }></i> </span>
             </span>
-            <div className=''>
-
+            <div className='pc-user-social-info'>
+              <div>
+                <div>动态</div>
+                <div>{ user.profile.eventCount }</div>
+              </div>
+              <div>
+                <div>关注</div>
+                <div>{ user.profile.follows }</div>
+              </div>
+              <div>
+                <div>粉丝</div>
+                <div>{ user.profile.followeds }</div>
+              </div>
             </div>
           </div>
         </div>
