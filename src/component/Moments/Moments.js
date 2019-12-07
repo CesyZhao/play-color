0d06ff99821ed31f41aab3e3860dabf1a3ff579e
Chris Zhao <997010638@qq.com>
2019-12-07 15:44:00 +0800

    朋友圈

--- src/component/Moments/Moments.js
+++ src/component/Moments/Moments.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import http from '../../config/http'
 import './Moments.less'
+import emojiConverter from '../../util/emoji'
 
 class Moments extends Component {
 
@@ -25,19 +26,81 @@ class Moments extends Component {
     })
   }
 
+  renderContent = (message) => {
+    function renderContent ({ img, name, author }) {
+      return (
+        <div className="pc-moment-content">
+          <img src={ img }></img>
+          <div className="pc-moment-content-info">
+            <div className="pc-moment-content-name">{ name }</div>
+            <div>{ author }</div>
+          </div>
+        </div>
+      )
+    }
+    function renderSong (song) {
+      return renderContent({
+        img: song.album.picUrl,
+        name: song.name,
+        author: song.artists.map(artist => artist.name).join('/')
+      })
+    }
+    function renderPlaylist (playlist) {
+      return renderContent({
+        img: playlist.coverImgUrl,
+        name: playlist.name,
+        author: playlist.creator.nickname
+      })
+    }
+    function renderVideo (video) {
+      return renderContent({
+        img: video.coverUrl,
+        name: video.title,
+        author: video.creator.nickname
+      })
+    }
+    function renderTopic (topic) {
+      return renderContent({
+        img: topic.rectanglePicUrl,
+        name: topic.title,
+        author: topic.creator.nickname
+      })
+    }
+    const contentMap = {
+      song: renderSong,
+      playlist: renderPlaylist,
+      video: renderVideo,
+      topic: renderTopic
+    }
+    for (const type in contentMap) {
+      if (message[type]) {
+        return contentMap[type](message[type])
+      }
+    }
+  }
+
   render () {
     return (
       <div className="pc-moments">
         <ul className="pc-moments-list">
           {
             this.state.moments.map(moment =>{
+              const { commentThread } = moment.info
+              const { resourceTitle } = commentThread
+              const [eventName, targetName] = resourceTitle ? resourceTitle.split('：') : []
               return (
                 <li className="pc-moments-item">
                   <img className="avatar" src={ moment.user.avatarUrl }></img>
                   <div className="pc-moments-item-content">
-                    <Link to={ `/user/${moment.user.userId}` }>{ moment.user.nickname }</Link>
+                    <div>
+                      <Link to={ `/user/${moment.user.userId}` } className="moment-user">{ moment.user.nickname }</Link>
+                      <span> { eventName } </span>
+                    </div>
                     <div className="pc-moment-time">{ new Date(moment.eventTime).toLocaleString() }</div>
-                    <div className="pc-moment-message">{ JSON.parse(moment.json).msg }</div>
+                    <div className="pc-moment-message">{ emojiConverter(JSON.parse(moment.json).msg) }</div>
+                    {
+                      this.renderContent(JSON.parse(moment.json))
+                    }
                     <div>
                       {
                          moment.pics.map(pic => {
@@ -46,10 +109,13 @@ class Moments extends Component {
                       }
                     </div>
                     <div className='pc-moment-item-footer'>
+                      <span></span>
                       <span>
-                        <span>
+                        <span className="pc-moment-options">
                           <i className='iconfont icon-zan'></i>
                           { moment.info.likedCount }
+                          <i className='iconfont icon-share_icon'></i>
+                          { moment.info.shareCount }
                         </span>
                       </span>
                     </div>
