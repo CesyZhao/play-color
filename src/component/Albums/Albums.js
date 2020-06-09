a2f6bcf8a66073640b029b9564584414bb9e9930
Chris Zhao <997010638@qq.com>
2020-06-09 08:51:04 +0800

    全部歌单页面

--- src/component/Albums/Albums.js
+++ src/component/Albums/Albums.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react'
 import './Albums.less'
 import api from '../../config/api'
 import toaster from '../../util/toast'
+import {Link} from 'react-router-dom'
+import LazyImage from '../LazyImage/LazyImage'
 // import _ from 'lodash'
 
 class Albums extends Component {
 
   state = {
     catList: [],
-    list: [],
+    lists: [],
     category: '',
     currentPage: 0,
     currentCategory: '全部'
@@ -25,12 +27,12 @@ class Albums extends Component {
   }
 
   getPlaylistByCategory = async () => {
-    const {list, currentCategory} = this.state
-    const before = list.length ? list.pop().updateTime : undefined
+    const {lists, currentCategory} = this.state
+    const before = lists.length ? lists.pop().updateTime : undefined
     try {
       const { data } = await api.song.getPlayLists({ before, category: currentCategory })
       this.setState({
-        list: data.playlists
+        lists: data.playlists
       })
     } catch (e) {
       toaster.error('获取歌单失败')
@@ -44,7 +46,7 @@ class Albums extends Component {
   }
 
   render() {
-    const { currentCategory } = this.state
+    const { currentCategory, catList, lists } = this.state
     return (
       <div className="pc-albums">
         <div className="pc-albums-categories">
@@ -52,10 +54,25 @@ class Albums extends Component {
             全部
           </span>
           {
-            this.state.catList.map(tag => {
-              return <span className={`pc-albums-hot-category ${currentCategory === tag.name && 'active'}`} key={tag.id} onClick={() => this.handleCategoryClick(tag.name)}>
-                {tag.name}
-              </span>
+            catList.map(tag => {
+              return (
+                <span className={`pc-albums-hot-category ${currentCategory === tag.name && 'active'}`} key={tag.id} onClick={() => this.handleCategoryClick(tag.name)}>
+                  {tag.name}
+                </span>
+              )
+            })
+          }
+        </div>
+        <div className="pc-albums-lists">
+          {
+            lists.map(list => {
+              return (
+                <Link key={list.id} to={{pathname: `/album/${list.id}`}}>
+                  <div className="pc-albums-album" data-name={list.name}>
+                    <LazyImage imgUrl={list.coverImgUrl} />
+                  </div>
+                </Link>
+              )
             })
           }
         </div>
